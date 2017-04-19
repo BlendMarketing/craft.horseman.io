f644c585f03243833c286cfc70543664033d5133
Jared Meyering <jared.meyering@gmail.com>
2017-04-19 13:52:17 -0500

    update the elementapi

--- config/elementapi.php
+++ config/elementapi.php
@@ -2,30 +2,48 @@
 namespace Craft;
 
 return [
-    'defaults' => [
-        'elementsPerPage' => 10,
-        'elementType' => 'Entry',
+    "defaults" => [
+        "elementsPerPage" => 10,
+        "elementType" => "Entry",
     ],
-    'endpoints' => [
-        craft()->request->path => function(){
+    "endpoints" => [
+        "components/<slug:{slug}>.json" => function($slug){
+
             HeaderHelper::setHeader([
-                'Access-Control-Allow-Origin' => 'http://horseman.dev'
+                "Access-Control-Allow-Origin" => "http://horseman.dev"
             ]);
 
-            $segments = craft()->request->getSegments();
-            $slug = $segments[count($segments) - 1];
+
             return [
-                'criteria' => [
-                    'section' => "pages",
-                    'slug' => $slug,
+                "criteria" => [
+                    "section" => "components",
+                    "slug" => $slug,
                 ],
-                'paginate' => false,
-                'first' => true,
-                'transformer' => function(EntryModel $entry) {
+                "paginate" => false,
+                "first" => true,
+                "transformer" => function(EntryModel $entry) {
+
+                    $authors = [];
+                    $components = [];
+                    foreach ($entry->authors as $author) {
+                        $authors[] = $author->title;
+                    }
+
+                    foreach ($entry->components as $block) {
+                        $blockValues = [];
+                        $fieldList = $block->getFieldLayout()->getFields();
+                        foreach ($fieldList as $field) {
+                            $handle = $field->getField()->handle;
+                            $blockValues[$handle] = $block->{$handle};
+                        }
+                        $components[] = $blockValues;
+                    }
+
                     return [
-                        'title' => $entry->title,
-                        'id' => $entry->id,
-                        'body' => $entry->body ? $entry->body->getParsedContent() : "",
+                        "title" => $entry->title,
+                        "id" => $entry->id,
+                        "authors" => $authors,
+                        "components" => $components,
                     ];
                 },
             ];
