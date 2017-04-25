46045c1927e06cd859efb7aac7fa67a86320fd20
Jared Meyering <jared.meyering@gmail.com>
2017-04-25 01:40:05 -0500

    add listing resource endpoint

--- config/elementapi.php
+++ config/elementapi.php
@@ -1,13 +1,51 @@
 <?php
 namespace Craft;
 
+$componentTransformer = function(EntryModel $entry) {
+
+    $authors = [];
+    $components = [];
+    foreach ($entry->authors as $author) {
+        $authors[] = $author->title;
+    }
+
+    foreach ($entry->components as $block) {
+        $blockValues = [];
+        $fieldList = $block->getFieldLayout()->getFields();
+        foreach ($fieldList as $field) {
+            $handle = $field->getField()->handle;
+            $blockValues[$handle] = $block->{$handle};
+        }
+        $components[] = $blockValues;
+    }
+
+    return [
+        "title" => $entry->title,
+        "id" => $entry->id,
+        "authors" => $authors,
+        "components" => $components,
+    ];
+};
+
+
 return [
     "defaults" => [
         "elementsPerPage" => 10,
         "elementType" => "Entry",
     ],
     "endpoints" => [
-        "components/<slug:{slug}>.json" => function($slug){
+        "components.json" => function() use ($componentTransformer) {
+            HeaderHelper::setHeader([
+                "Access-Control-Allow-Origin" => "http://horseman.dev"
+            ]);
+            return [
+                "criteria" => [
+                    "section" => "components",
+                ],
+                "transformer" => $componentTransformer,
+            ];
+        },
+        "components/<slug:{slug}>.json" => function($slug) use ($componentTransformer) {
 
             HeaderHelper::setHeader([
                 "Access-Control-Allow-Origin" => "http://horseman.dev"
@@ -21,31 +59,7 @@ return [
                 ],
                 "paginate" => false,
                 "first" => true,
-                "transformer" => function(EntryModel $entry) {
-
-                    $authors = [];
-                    $components = [];
-                    foreach ($entry->authors as $author) {
-                        $authors[] = $author->title;
-                    }
-
-                    foreach ($entry->components as $block) {
-                        $blockValues = [];
-                        $fieldList = $block->getFieldLayout()->getFields();
-                        foreach ($fieldList as $field) {
-                            $handle = $field->getField()->handle;
-                            $blockValues[$handle] = $block->{$handle};
-                        }
-                        $components[] = $blockValues;
-                    }
-
-                    return [
-                        "title" => $entry->title,
-                        "id" => $entry->id,
-                        "authors" => $authors,
-                        "components" => $components,
-                    ];
-                },
+                "transformer" => $componentTransformer,
             ];
         },
     ]
